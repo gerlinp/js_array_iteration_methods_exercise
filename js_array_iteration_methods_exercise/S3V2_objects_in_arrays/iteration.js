d8f03e9239e26293ef679c79dd91aecd7acdbc4b
gerlinp <gerlin.pierrelouis@gmail.com>
2021-05-04 22:49:14 -0400

    Working with Objects in Arrays

--- js_array_iteration_methods_exercise/S3V2_objects_in_arrays/iteration.js
+++ js_array_iteration_methods_exercise/S3V2_objects_in_arrays/iteration.js
@@ -1,5 +1,13 @@
 const users = [
-  {name: 'Samir'},
-  {name: 'Angela'},
-  {name: 'Beatrice'}
+  {name: 'Samir', age: 27},
+  {name: 'Angela',age: 33},
+  {name: 'Beatrice',age: 42}
 ];
+
+
+const usersObject = users.reduce((usersObject, user) => {
+  usersObject[user.name] = user.age;
+  return usersObject;
+}, {})
+
+console.log(usersObject);
